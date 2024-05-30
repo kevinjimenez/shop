4105603515eb2f73ee4b005cf04a9561f9813793
Kevin Jimenez <orlando.kj24@gmail.com>
2024-05-29 20:58:45 -0500

    feat: 🚗 creacion de componentes iniciales y definicion rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import LayoutComponent from './layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'main',
+        loadComponent: () =>
+          import('./main/main.component').then((m) => m.default),
+      },
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.routes').then((m) => m.routes),
+      },
+      {
+        path: 'sign-in',
+        loadComponent: () =>
+          import('./login/login.component').then((m) => m.default),
+      },
+      {
+        path: 'sign-up',
+        loadComponent: () =>
+          import('./register/register.component').then((m) => m.default),
+      },
+      {
+        path: '',
+        redirectTo: 'main',
+        pathMatch: 'full',
+      },
+    ],
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./not-found/not-found.component').then((m) => m.default),
+  },
+];
