c7b4fc02371bc184f8f1fe70f2b4c4d807e545b2
Kevin Jimenez <orlando.kj24@gmail.com>
2024-06-07 17:16:28 -0500

    feat: 🚧 ambientes dev y prod

--- src/app/core/services/categories.service.ts
+++ src/app/core/services/categories.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 import { ErrorResponse } from '../models/error-response.mode';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -10,8 +11,9 @@ export class CategoriesService {
   private readonly _httpClient = inject(HttpClient);
 
   getAll() {
+    const url = `${environment.apiUrl}/products/categories`;
     return this._httpClient
-      .get<string[]>('https://fakestoreapi.com/products/categories')
+      .get<string[]>(url)
       .pipe(catchError(this.handleErrorResponse));
   }
 
--- src/app/core/services/products.service.ts
+++ src/app/core/services/products.service.ts
@@ -3,6 +3,7 @@ import { Injectable, inject } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 import { ErrorResponse } from '../models/error-response.mode';
 import { Product } from '../models/product.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -11,21 +12,24 @@ export class ProductsService {
   readonly #httpClient = inject(HttpClient);
 
   getAll(limit = 5) {
+    const url = `${environment.apiUrl}/products?limit=${limit}`;
     return this.#httpClient
-      .get<Product[]>(`https://fakestoreapi.com/products?limit=${limit}`)
+      .get<Product[]>(url)
       .pipe(catchError(this.handleErrorResponse));
   }
 
   getById(id: number) {
+    const url = `${environment.apiUrl}/products/${id}`;
     return this.#httpClient
-      .get<Product>('https://fakestoreapi.com/products/' + id)
+      .get<Product>(url)
       .pipe(catchError(this.handleErrorResponse));
   }
 
   getByCategory(category?: string) {
+    const url = `${environment.apiUrl}/products/category/${category}`;
     if (!category) return this.getAll();
     return this.#httpClient
-      .get<Product[]>(`https://fakestoreapi.com/products/category/${category}`)
+      .get<Product[]>(url)
       .pipe(catchError(this.handleErrorResponse));
   }
 
