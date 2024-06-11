0001fd081f77fba4b25dddcda1baf2fdd7f99f37
Kevin Jimenez <orlando.kj24@gmail.com>
2024-06-11 14:09:27 -0500

    feat: 🚧 sidenav para carrito

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -1,6 +1,6 @@
-import { Overlay, OverlayModule } from '@angular/cdk/overlay';
+import { OverlayModule } from '@angular/cdk/overlay';
 import { CurrencyPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
 import { ImageComponent } from '../../../shared/components/image/image.component';
 import { MoneyBagComponent } from '../../../shared/components/svg/money-bag.component';
 import { CartStore } from '../../../store/project.store';
@@ -13,8 +13,7 @@ import { CartStore } from '../../../store/project.store';
 })
 export class HeaderComponent {
   readonly cartStore = inject(CartStore);
-  readonly overlay = inject(Overlay);
-  public s = this.overlay.scrollStrategies.block();
-  public isOpen = false;
+  public openCart = output();
+
   public productsCount = this.cartStore.products.length;
 }
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { MatDrawerMode, MatSidenavModule } from '@angular/material/sidenav';
+import { CartStore } from '../store/project.store';
+import { ButtonComponent } from '../shared/components/button/button.component';
+import { ShoppingCartOffComponent } from '../shared/components/svg/shopping-cart-off.component';
+import { ImageComponent } from '../shared/components/image/image.component';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    MatSidenavModule,
+    ButtonComponent,
+    ShoppingCartOffComponent,
+    ImageComponent,
+    CurrencyPipe,
+  ],
   templateUrl: './layout.component.html',
 })
-export default class LayoutComponent {}
+export default class LayoutComponent {
+  readonly cartStore = inject(CartStore);
+  public mode = signal<MatDrawerMode>('over');
+}
